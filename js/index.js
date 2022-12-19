3b9f21689287c5ad62e01bc330b03ef0051f05af
Sofyan Saif Pahlevi <114819024+SofyanSaif@users.noreply.github.com>
2022-12-19 10:06:45 +0700

    Add files via upload

---
+++ js/index.js
@@ -0,0 +1,31 @@
+// untuk mengambil sebuah value pada inputan yang ada di HTML tentunya dengan syarat element inputan tersebut memiliki ID
+document.getElementById("myButton").onclick = function(){
+
+    // menampilkan pop up untuk memasukan jumlah pesanan
+         var jml = prompt("Masukkan Jumlah Pesanan :  ");
+         // rumus untuk menghitung jumlah pesanan dikali harga 
+         var jumlah =   jml * 17000;
+         // jika produk yang dibeli lebih dari sama dengan 10 maka proses pembayaran akan berlanjut
+         if (jml >= 20 ) {
+           // menampikan tulisan di halaman web
+             document.write(`Produk yang anda pesan adalah  : <b>Ribena </b><br>`);
+             // menampilkan jumlah barang yang diinputkan pada variabel jml di halaman web
+             document.write(`Jumlah pesanan anda : <b>${jml}</b><br>`);
+              // menampilkan total harga yang sudah dihitung pada variabel jumlah di halaman web
+             document.write(`Total harga : Rp <b>${jumlah}</b>`);
+             
+         }
+         // jika jumlah produk yang dipesan tidak sesuai makan proses pembayaran gagal
+         else   {
+             // menampikan pop up berupa tulisan saja dan tidak ada kolom input
+             alert('Minimal pembelian tidak sesuai');
+             // mengelinkan tombol "ok" yang ditekan ke halaman ribena.html
+             document.link("ribena.html");
+             
+             
+         } 
+         document.write("<p> Terima Kasih dah mampir <p>");
+             document.write("<p> Untuk melanjutkan tranksasi silahkan ss halaman ini dan kirim wa di link di bawah ini <p>");
+             document.write("<a href= 'http://wa.me/6287860462909'>Link WA</a>");
+             document.write(" <a href= 'ribena.html'>Back To Page</a>")
+ }
---
+++ js/index8.js
@@ -0,0 +1,29 @@
+document.getElementById("myButton").onclick = function(){
+
+    // menampilkan pop up untuk memasukan jumlah pesanan
+         var jml = prompt("Masukkan Jumlah Pesanan :  ");
+         // rumus untuk menghitung jumlah pesanan dikali harga 
+         var jumlah =   jml * 22000;
+         // jika produk yang dibeli lebih dari sama dengan 10 maka proses pembayaran akan berlanjut
+         if (jml >= 20 ) {
+           // menampikan tulisan di halaman web
+             document.write(`Produk yang anda pesan adalah  : <b>Korean Goodpoki Cup</b><br>`);
+             // menampilkan jumlah barang yang diinputkan pada variabel jml di halaman web
+             document.write(`Jumlah pesanan anda : <b>${jml}</b><br>`);
+              // menampilkan total harga yang sudah dihitung pada variabel jumlah di halaman web
+             document.write(`Total harga : Rp <b>${jumlah}</b>`);
+             
+         }
+         // jika jumlah produk yang dipesan tidak sesuai makan proses pembayaran gagal
+         else   {
+           // menampikan pop up berupa tulisan saja dan tidak ada kolom input
+             alert('Minimal pembelian tidak sesuai');
+             // mengelinkan tombol "ok" yang ditekan ke halaman celana.html
+             document.location.href = "ghaurmilk.html";
+             
+             
+         }
+         document.write("<p> Terima Kasih dah mampir <p>");
+             document.write("<p> Untuk melanjutkan tranksasi silahkan ss halaman ini dan kirim wa di link di bawah ini <p>");
+             document.write("<a href= 'http://wa.me/6287860462909'>Link WA</a>");
+ }
